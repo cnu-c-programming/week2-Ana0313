67aa080269c7a503bcb5c6e39dd6bb229f16d6ac
Ana0313 <rokafosh2@gmail.com>
2026-03-19 20:00:43 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,35 @@
 
 int main()
 {
+    int num;
+    int isPrime = 1; 
+  
+    scanf("%d", &num);
+
+    if (num <= 1) 
+    {
+        isPrime = 0; 
+    }
+    else 
+    {
+        for (int i = 2; i < num; i++) 
+        {
+            if (num % i == 0) 
+            {
+                isPrime = 0; 
+                break;      
+            }
+        }
+    }
+
+    if (isPrime == 1) 
+    {
+        printf("true\n");
+    } 
+    else 
+    {
+        printf("false\n");
+    }
 
     return 0;
 }
-
