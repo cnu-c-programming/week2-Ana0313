279dfd6734090bfdab1d3859b669066e69e3ccd0
Ana0313 <rokafosh2@gmail.com>
2026-03-19 17:28:21 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,33 @@
 
 int main()
 {
+    int num1, num2;
+    char op; 
+
+    scanf("%d %d %c", &num1, &num2, &op);
+
+    switch (op)
+    {
+        case '+':
+            printf("%d\n", num1 + num2);
+            break;
+        case '-':
+            printf("%d\n", num1 - num2);
+            break;
+        case '*':
+            printf("%d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%d\n", num1 / num2);
+            } else {
+                printf("0으로 나눌 수 없습니다.\n");
+            }
+            break;
+        default:
+            printf("잘못된 연산자입니다.\n");
+            break;
+    }
 
     return 0;
 }
-
